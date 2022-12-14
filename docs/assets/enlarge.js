f7ef4c4176355d9f0a538caba8cbec88ce8ccb7a
will <will@invertedv.com>
2022-12-13 22:37:43 -0700

    approach.md #A18

--- docs/assets/enlarge.js
+++ docs/assets/enlarge.js
@@ -1,17 +1,16 @@
-    var state=0;
-    var zInd = 0;
-    function enlargeImg(img, shift) {
-        alert("here");
-    if (state == 0) {
+var state=0;
+var zInd = 0;
+function enlargeImg(img, shift) {
+  if (state == 0) {
     state=1;
     img.style.transform = "scale(2.5)";
     img.style.position = "relative";
     img.style.left = shift + "px";
     zInd++;
     img.style.zIndex = String(zInd);
-} else {
+  } else {
     state=0;
     img.style.transform = "scale(1.0)";
     img.style.left = "0px";
-}
+  }
 }
